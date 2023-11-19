48b592d1ddf40c9343ffe5ed4b4fa06f158005d4
Mark Aldwin Handumon <handumonmark123@gmail.com>
2023-11-19 13:35:47 +0800

    ActObjects

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, Modal, ScrollView, Button, Image } from 'react-native';
+import { styles } from './styles/style';
 
-export default function App() {
+const ImageEvent = [
+  {
+    firstName: 'Chariz',
+    lastName: 'Baco',
+    nickname: 'Gwapa',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/aa.jpg'), 
+  },
+  {
+    firstName: 'James',
+    lastName: 'Noble',
+    nickname: 'Mage User',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/bb.jpg'), 
+  },
+  {
+    firstName: 'John Carlo',
+    lastName: 'Razon',
+    nickname: 'Kicker',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/cc.jpg'), 
+  },
+  {
+    firstName: 'Rodel',
+    lastName: 'Jawa',
+    nickname: 'Tank User',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/dd.jpg'), 
+  },
+  {
+    firstName: 'Jovert',
+    lastName: 'Davirao',
+    nickname: 'Verting',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/ee.jpg'), 
+  },
+  {
+    firstName: 'Jess Raphael',
+    lastName: 'Jumawan',
+    nickname: 'Crazy Guy',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/ff.jpg'), 
+  },
+  {
+    firstName: 'John Mark',
+    lastName: 'Lagrimas',
+    nickname: 'Cute Roller',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/gg.jpg'), 
+  },
+  {
+    firstName: 'Mc Aldwin',
+    lastName: 'Handumon',
+    nickname: 'Expert',
+    course: 'BSIT',
+    year: 'Third Year',
+    image: require('./assets/hh.jpg'), 
+  },
+];
+
+const Numberlists = () => {
+  const [selectedUser, setSelectedUser] = React.useState(null);
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => {
+      setSelectedUser(item);
+      setModalVisible(true);
+    }}>
+      <Text style={styles.nickname}>{item.nickname}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Text style={styles.pt}>TEAM GEEKS OF IT</Text>
+      <FlatList
+        data={ImageEvent}
+        keyExtractor={(item) => item.nickname}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          {selectedUser && (
+            <ScrollView>
+              <Text style={styles.fullName}>
+                {selectedUser.firstName} {selectedUser.lastName}
+              </Text>
+              <Text>Nickname: {selectedUser.nickname}</Text>
+              <Text>Course: {selectedUser.course}</Text>
+              <Text>Year: {selectedUser.year}</Text>
+              {selectedUser.image && <Image source={selectedUser.image} style={{ width: 200, height: 200 }} />}
+              <Button title="Close" onPress={() => setModalVisible(!modalVisible)} />
+            </ScrollView>
+          )}
+        </View>
+      </Modal>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Numberlists;
